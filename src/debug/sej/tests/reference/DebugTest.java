119d4f882dc4b64881c2b1246aa59d88e2a0e8fb
Peter Arrenbrecht <parren@users.noreply.github.com>
2006-08-30 17:40:57 +0200

    [svn] Added new debug test based on reference test

---
+++ src/debug/sej/tests/reference/DebugTest.java
@@ -0,0 +1,41 @@
+package sej.tests.reference;
+
+import java.io.IOException;
+
+import sej.SaveableEngine;
+import sej.internal.Debug;
+import sej.internal.Settings;
+
+
+public final class DebugTest extends AbstractReferenceTest
+{
+
+	static {
+		Settings.setDebugLogEnabled( true );
+	}
+
+	public DebugTest()
+	{
+		super( "Strings" );
+		//super( "Strings", 10, NumType.LONG, 0, false );
+	}
+	
+	@Override
+	protected void reportTestRun( String _testName )
+	{
+		System.out.println( _testName );
+	}
+
+	@Override
+	protected void reportDefectiveEngine( SaveableEngine _engine, String _testName )
+	{
+		try {
+			Debug.saveEngine( _engine, "d:/temp/debug.jar" );
+			System.out.println( ".. dumped to d:/temp/debug.jar" );
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
