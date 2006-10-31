a93163beb0c5b4a6595bbd05c53db4d8740da5ca
peter arrenbrecht <peter.arrenbrecht@gmail.com>
2006-10-31 09:36:32 +0100

    [svn] Build works using bootstrap dummies for initial compilation of internal code generators. Bumped version to 0.8.0

---
+++ src/classes-bootstrap-dummies/sej/internal/bytecode/compiler/ExpressionCompilerForAll_Generated.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright © 2006 by Abacus Research AG, Switzerland.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are prohibited, unless you have been explicitly granted 
+ * more rights by Abacus Research AG.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * DO NOT MODIFY! This file is generated automatically from sej.internal.templates.ExpressionTemplatesForAll.
+ */
+package sej.internal.bytecode.compiler;
+
+import org.objectweb.asm.Label;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.GeneratorAdapter;
+
+import sej.CompilerException;
+import sej.Operator;
+import sej.NumericType;
+import sej.internal.expressions.ExpressionNode;
+import sej.internal.expressions.ExpressionNodeForFunction;
+
+/**
+ * This is a dummy class for boot-strap compilation of the class that will compile the actual
+ * version of this class. It is only used during the initial compile run.
+ */
+@SuppressWarnings("unused")
+abstract class ExpressionCompilerForAll_Generated extends ExpressionCompilerForAll_Base
+{
+
+	public ExpressionCompilerForAll_Generated(MethodCompiler _methodCompiler)
+	{
+		super( _methodCompiler );
+	}
+
+	protected final void compile_util_unboxByte() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_unboxShort() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_unboxInteger() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_unboxLong() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_unboxFloat() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_unboxDouble() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_unboxCharacter() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_unboxBoolean() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_boxByte() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_boxShort() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_boxInteger() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_boxLong() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_boxFloat() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_boxDouble() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_boxCharacter() throws CompilerException
+	{
+		// dummy
+	}
+
+	protected final void compile_util_boxBoolean() throws CompilerException
+	{
+		// dummy
+	}
+
+	@Override
+	protected final void compile_scanArray( ForEachElementCompilation _forElement ) throws CompilerException
+	{
+		// dummy
+	}
+
+	@Override
+	protected final void compile_scanArrayWithFirst( ForEachElementWithFirstCompilation _forElement )
+			throws CompilerException
+	{
+		// dummy
+	}
+
+}
