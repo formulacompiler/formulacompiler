0e9973d38fb2257ec304b0fef298081c04da42b1
peter arrenbrecht <peter.arrenbrecht@gmail.com>
2006-12-11 11:51:32 +0100

    [svn] Free-form criteria for DSUM

--- src/debug/sej/tests/reference/DebugDatabaseTableTest.java
+++ src/debug/sej/tests/reference/DebugDatabaseTableTest.java
@@ -51,7 +51,7 @@ public final class DebugDatabaseTableTest extends AbstractDatabaseTableTest
 	 */
 	public DebugDatabaseTableTest()
 	{
-		super( "DatabaseAggregators", 23, NumType.DOUBLE, Integer.valueOf( "1", 2 ), false );
+		super( "DatabaseAggregators", 31, NumType.DOUBLE, Integer.valueOf( "0", 2 ), false );
 	}
 
 
