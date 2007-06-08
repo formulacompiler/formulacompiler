ce917ab2614388cc6cf68eb1858b7ea049bdbeb7
peter arrenbrecht <peter.arrenbrecht@gmail.com>
2007-06-08 16:07:17 +0200

    [svn] Removed trunk so I can upload the new layout.

--- src/doc/_funcs.rb
+++
@@ -1,9 +0,0 @@
-# Global utility functions for Rextile.
-
-def inc_file(name)
-    read_file name
-rescue
-    msg = "#{name} missing in #{Dir.getwd}? Error was #{$!}."
-    warn msg
-    msg
-end
--- src/doc/reference/_settings.rb
+++
@@ -1,4 +0,0 @@
-@crumbs << 'Reference'
-
-@head_nodes << '<link rel="stylesheet" type="text/css" href="refstyle.css" />'
-
