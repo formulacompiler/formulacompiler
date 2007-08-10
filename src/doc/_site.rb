699e5b7e92026afad8ba2e3a58e5b0ed2609cbf0
Peter Arrenbrecht <parren@users.noreply.github.com>
2007-08-10 09:02:06 +0200

    [svn] Added preliminary support for Rextile 1.6's ability to only rebuild changed pages.

--- src/doc/_site.rb
+++ src/doc/_site.rb
@@ -5,3 +5,7 @@ require '_funcs.rb'
 
 # Use Rextile's default template.
 @template_path = "#{INSTALL_PATH}/templates/standard"
+
+# Keep make-dependency information
+@dependency_info = "../../temp/rextile.deps"
+
