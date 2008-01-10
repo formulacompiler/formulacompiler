0ff11f6079ff37fd973c154f410fc7f2ae7d4feb
Peter Arrenbrecht <parren@users.noreply.github.com>
2008-01-10 16:31:46 +0100

    Fix unicode tests so they work again with the changed createCellNamesFromRowTitles().

--- components/spreadsheet.opendocument/src/test/java/org/formulacompiler/spreadsheet/internal/loader/odf/UnicodeTest.java
+++ components/spreadsheet.opendocument/src/test/java/org/formulacompiler/spreadsheet/internal/loader/odf/UnicodeTest.java
@@ -20,15 +20,16 @@
  */
 package org.formulacompiler.spreadsheet.internal.loader.odf;
 
+import java.io.File;
+
 import org.formulacompiler.spreadsheet.EngineBuilder;
 import org.formulacompiler.spreadsheet.Spreadsheet;
 import org.formulacompiler.spreadsheet.SpreadsheetCompiler;
-
-import junit.framework.TestCase;
+import org.formulacompiler.tests.utils.AbstractIOTestCase;
 
 
 @SuppressWarnings("unqualified-field-access")
-public class UnicodeTest extends TestCase
+public class UnicodeTest extends AbstractIOTestCase
 {
 	private Spreadsheet spreadsheet;
 
@@ -42,10 +43,15 @@ public class UnicodeTest extends TestCase
 		spreadsheet = builder.getSpreadsheet();
 	}
 
+	public void testYaml() throws Exception
+	{
+		assertYaml( new File( "src/test/data" ), "UnicodeTest", spreadsheet, "UnicodeTest.ods" );
+	}
+
 	public void testBasicLatinSymbols()
 	{
 		final String expected = " !\"#$%&'()*+,-./:;<=>?@{|}~";
-		final Spreadsheet.Cell cell = spreadsheet.getCell( "Basic Latin Symbols" );
+		final Spreadsheet.Cell cell = spreadsheet.getCell( "BasicLatinSymbols" );
 		final String actual = (String) cell.getConstantValue();
 		assertEquals( expected, actual );
 	}
@@ -53,7 +59,7 @@ public class UnicodeTest extends TestCase
 	public void testBasicLatinCharacters()
 	{
 		final String expected = "ABCDXYZabcdxyz";
-		final Spreadsheet.Cell cell = spreadsheet.getCell( "Basic Latin Characters" );
+		final Spreadsheet.Cell cell = spreadsheet.getCell( "BasicLatinCharacters" );
 		final String actual = (String) cell.getConstantValue();
 		assertEquals( expected, actual );
 	}
@@ -61,7 +67,7 @@ public class UnicodeTest extends TestCase
 	public void testLatin1Symbols()
 	{
 		final String expected = "\u00A0\u00A1\u00A2\u00A3\u00A4\u00A5\u00A6\u00A7\u00A8\u00A9\u00AA\u00AB\u00AC\u00AD\u00AE\u00AF\u00B0\u00B1\u00B2\u00B3\u00B4\u00B5\u00B6\u00B7\u00B8\u00B9\u00BA\u00BB\u00BC\u00BD\u00BE\u00BF";
-		final Spreadsheet.Cell cell = spreadsheet.getCell( "Latin-1 Symbols" );
+		final Spreadsheet.Cell cell = spreadsheet.getCell( "Latin1Symbols" );
 		final String actual = (String) cell.getConstantValue();
 		assertEquals( expected, actual );
 	}
@@ -69,7 +75,7 @@ public class UnicodeTest extends TestCase
 	public void testLatin1Characters()
 	{
 		final String expected = "\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5\u00C6\u00C7\u00DD\u00DE\u00DF\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5\u00E6\u00E7\u00FD\u00FE\u00FF";
-		final Spreadsheet.Cell cell = spreadsheet.getCell( "Latin-1 Characters" );
+		final Spreadsheet.Cell cell = spreadsheet.getCell( "Latin1Characters" );
 		final String actual = (String) cell.getConstantValue();
 		assertEquals( expected, actual );
 	}
@@ -77,7 +83,7 @@ public class UnicodeTest extends TestCase
 	public void testCyrillicCharacters()
 	{
 		final String expected = "\u0401\u0404\u0406\u0407\u0410\u0411\u0412\u0413\u0414\u0430\u0431\u0432\u0433\u0434\u0451\u0454\u0456\u0457\u04E8\u04E9";
-		final Spreadsheet.Cell cell = spreadsheet.getCell( "Cyrillic Characters" );
+		final Spreadsheet.Cell cell = spreadsheet.getCell( "CyrillicCharacters" );
 		final String actual = (String) cell.getConstantValue();
 		assertEquals( expected, actual );
 	}
@@ -85,7 +91,7 @@ public class UnicodeTest extends TestCase
 	public void testMixedCharacters()
 	{
 		final String expected = "\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5\u0410\u0411\u0412\u0413\u0414\u0430\u0431\u0432\u0433\u0434";
-		final Spreadsheet.Cell cell = spreadsheet.getCell( "Mixed Characters" );
+		final Spreadsheet.Cell cell = spreadsheet.getCell( "MixedCharacters" );
 		final String actual = (String) cell.getConstantValue();
 		assertEquals( expected, actual );
 	}
