917a6de96d8020350f3d9945e6de1f12d7c66c0d
Peter Arrenbrecht <parren@users.noreply.github.com>
2008-02-21 11:24:44 +0100

    doc: Removed rounded corners as they did not work with IE.

--- src/doc/download/versions.rb
+++ src/doc/download/versions.rb
@@ -3,9 +3,6 @@ def ver( afc_ver, tools_ver )
 	return %Q[
 	
 <div class="download-box">
-<div class="rounded_box">
-<div class="rounded_top"><div></div></div>
-<div class="rounded_content">
 <p>
 
 dt. "formulacompiler-#{afc_ver}-bin.zip":http://arrenbrecht.ch/afc/dist/formulacompiler-#{afc_ver}-bin.zip : Binary release, including documentation and all required third-party libraries.
@@ -17,9 +14,6 @@ dt. "formulacompiler-#{afc_ver}-src.zip":http://arrenbrecht.ch/afc/dist/formulac
 dt. "formulacompiler-#{tools_ver}-tools.zip":http://arrenbrecht.ch/afc/dist/formulacompiler-#{tools_ver}-tools.zip : Most of the external tools required to build an AFC distribution in a single download. It's OK if the version here is less than the others.
 
 </p>
-</div>
-<div class="rounded_bot"><div></div></div>
-</div>
 </div>
 
 ]
