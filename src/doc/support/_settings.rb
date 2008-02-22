aa839339ddeb42c103a26b8a0ae2ed5f66ecbc63
Peter Arrenbrecht <peter.arrenbrecht@gmail.com>
2008-02-22 15:40:22 +0100

    doc: Move support page to its own subdir and improve its contents.

---
+++ src/doc/support/_settings.rb
@@ -0,0 +1,8 @@
+@crumbs << 'Support'
+
+add_topics([
+	'http://formulacompiler.org/trac/wiki', 'Wiki',
+	'http://groups.google.com/group/formulacompiler-users', 'Discussion Forum',
+	'http://formulacompiler.org/trac/report', 'Bug Reports',
+	])
+
