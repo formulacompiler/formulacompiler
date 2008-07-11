f193893b2969c627f486d3792c71d146e5b3a94d
Vladimir Korenev <vkorenev@gmail.com>
2008-07-11 20:32:42 +0300

    Upgrade JExcelApi to 2.6.8. Fix parsing of MDETERM.

--- components/spreadsheet.excel.xls/src/patches/jxl-2.6.8-afc-apply.sh
+++ components/spreadsheet.excel.xls/src/patches/jxl-2.6.8-afc-apply.sh
@@ -1,20 +1,15 @@
-#! /bin/bash
+#!/bin/bash
 
 # Location setup
 src=`echo $0 | sed s/-apply.sh//`
 
 # Sanity checks
 if [ ! -f $src.hgq ]; then echo "Could not find:" $src.hgq; exit; fi
-if [ ! -f $src.hgignore ]; then echo "Could not find:" $src.hgignore; exit; fi
 if [ ! -f jxlrwtest.xls ]; then echo "Don't seem to be in a JExcelAPI source folder; missing jxlrwtest.xls"; exit; fi
-if [ -a .hg ]; then echo "Current folder already is an hg repository (found .hg)."; exit; fi
+if [ ! -d .hg ]; then echo "Current folder is not a Mercurial repository (.hg not found)"; exit; fi
 
-# Init as hg repo
-hg init ; hg qinit -c
-
-# Copy .hgignore, then add unpacked files as rev 0
-cp $src.hgignore .hgignore
-hg add ; hg ci -m "Unpatched version of JExcelAPI."
+# Init mq
+hg qinit -c
 
 # Unbundle the patch queue
 hg -R .hg/patches unbundle --update $src.hgq
@@ -22,4 +17,3 @@ hg -R .hg/patches unbundle --update $src.hgq
 # Apply the patches, build and test
 hg qpush -a
 ( cd build ; ant test )
-
