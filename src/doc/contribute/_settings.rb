8f5513acc462ad4ae8e42862136ad6fdfe376076
Vladimir Korenev <vkorenev@users.noreply.github.com>
2009-06-04 19:01:56 +0300

    doc: Redirect links from http://formulacompiler.org/trac/ to http://code.google.com/p/formulacompiler/.

--- src/doc/contribute/_settings.rb
+++ src/doc/contribute/_settings.rb
@@ -2,8 +2,8 @@
 
 add_topics([
 	'http://groups.google.com/group/formulacompiler-users', 'Discussion Forum',
-	'http://formulacompiler.org/trac/wiki', 'Wiki',
-	'http://formulacompiler.org/trac/report', 'Bug Reports',
+	'http://code.google.com/p/formulacompiler/w/list', 'Wiki',
+	'http://code.google.com/p/formulacompiler/issues/list', 'Bug Reports',
 	:hacking_, 'Developer Documentation',
 	:copyright, 'Granting Of Rights',
 	:journal_, "Peter's Journal",
