8e6713bc018b14255a295d219833151b512b767b
Vladimir Korenev <vkorenev@gmail.com>
2009-06-04 19:01:56 +0300

    doc: Redirect links from http://formulacompiler.org/trac/ to http://code.google.com/p/formulacompiler/.

--- src/doc/support/_settings.rb
+++ src/doc/support/_settings.rb
@@ -1,8 +1,8 @@
 @crumbs << 'Support'
 
 add_topics([
-	'http://formulacompiler.org/trac/wiki', 'Wiki',
+	'http://code.google.com/p/formulacompiler/w/list', 'Wiki',
 	'http://groups.google.com/group/formulacompiler-users', 'Discussion Forum',
-	'http://formulacompiler.org/trac/report', 'Bug Reports',
+	'http://code.google.com/p/formulacompiler/issues/list', 'Bug Reports',
 	])
 
